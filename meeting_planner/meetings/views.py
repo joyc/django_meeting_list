f596be29eea720cbb4c489cb36c878d7f972ad8c
joyc <joyc@live.jp>
2020-04-20 00:13:37 +0900

    use template

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -6,12 +6,12 @@ from .models import Meeting, Room
 def detail(request, id):
     # meeting = Meeting.objects.get(pk=id)
     meeting = get_object_or_404(Meeting, pk=id)
-    return render(request, "meetings/detail.html", {"meeting": meeting})
+    return render(request, "detail.html", {"meeting": meeting})
 
 
 # add page of all room list
 # - view / template / url mapping
 def room_list(request):
-    return render(request, "meetings/room_list.html",
+    return render(request, "room_list.html",
                   {"rooms": Room.objects.all()})
 
