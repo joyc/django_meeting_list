491825e8f959618f1d16f51dafd8affabc3327da
joyc <joyc@live.jp>
2020-04-19 17:02:53 +0900

    Create template of index

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,15 +1,17 @@
 from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html")
 
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
+
 
 def about(request):
     return HttpResponse("This is a about page.")
